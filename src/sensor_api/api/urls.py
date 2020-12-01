3c9d9c8891950d10c08987e5f3bbd65a664f8271
nishant173 <nishant.rao173@gmail.com>
2020-12-01 15:17:13 +0530

    Refactored - Modified API endpoint names

--- src/sensor_api/api/urls.py
+++ src/sensor_api/api/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path(route='documentation', view=views.documentation, name='documentation'),
     path(route='sensor/records', view=views.all_sensor_records, name='all-sensor-records'),
+    path(route='sensor/records/<str:_id>', view=views.get_sensor_record, name='get-sensor-record'),
     path(route='sensor/records/filter', view=views.filter_sensor_records, name='filter-sensor-records'),
-    path(route='sensor/stats', view=views.sensor_stats, name='sensor-stats'),
-    path(route='sensor/record', view=views.get_sensor_record, name='get-sensor-record'),
-    path(route='sensor/record/add', view=views.add_sensor_record, name='add-sensor-record'),
-    path(route='sensor/record/delete', view=views.delete_sensor_record, name='delete-sensor-record'),
+    path(route='sensor/records/stats', view=views.sensor_stats, name='sensor-stats'),
+    path(route='sensor/records/add', view=views.add_sensor_record, name='add-sensor-record'),
+    path(route='sensor/records/delete/<str:_id>', view=views.delete_sensor_record, name='delete-sensor-record'),
 ]
--- src/sensor_api/api/views.py
+++ src/sensor_api/api/views.py
@@ -14,8 +14,15 @@ def documentation(request):
             'methods': ['GET'],
         },
         {
+            'endpoint': 'sensor/records/<str:_id>',
+            'description': 'Gets one sensor record by ID',
+            'parameters': [],
+            'example': 'sensor/records/IPKR8IF8BCVZ',
+            'methods': ['GET'],
+        },
+        {
             'endpoint': 'sensor/records/filter',
-            'description': 'Filters sensor records based on certain parameters',
+            'description': 'Filters sensor records (based on certain parameters)',
             'parameters': [
                 {
                     'name': 'sensorType',
@@ -49,8 +56,8 @@ def documentation(request):
             'methods': ['GET'],
         },
         {
-            'endpoint': 'sensor/stats',
-            'description': 'Gets sensor related statistics',
+            'endpoint': 'sensor/records/stats',
+            'description': 'Gets sensor related statistics (based on certain parameters)',
             'parameters': [
                 {
                     'name': 'startDate',
@@ -65,25 +72,12 @@ def documentation(request):
                     'required': True,
                 },
             ],
-            'example': 'sensor/stats?startDate=14-06-2020&endDate=31-12-2020',
+            'example': 'sensor/records/stats?startDate=14-06-2020&endDate=31-12-2020',
             'methods': ['GET'],
         },
         {
-            'endpoint': 'sensor/record',
-            'description': 'Gets one sensor record by ID',
-            'parameters': [
-                {
-                    'name': '_id',
-                    'datatype': 'str',
-                    'required': True,
-                },
-            ],
-            'example': 'sensor/record?_id=IPKR8IF8BCVZ',
-            'methods': ['GET'],
-        },
-        {
-            'endpoint': 'sensor/record/add',
-            'description': 'Adds one sensor record based on certain parameters',
+            'endpoint': 'sensor/records/add',
+            'description': 'Adds one sensor record (based on certain parameters)',
             'parameters': [
                 {
                     'name': 'reading',
@@ -96,20 +90,14 @@ def documentation(request):
                     'required': True,
                 },
             ],
-            'example': 'sensor/record/add?reading=10.224&sensorType=temperature',
+            'example': 'sensor/records/add?reading=10.224&sensorType=temperature',
             'methods': ['POST'],
         },
         {
-            'endpoint': 'sensor/record/delete',
+            'endpoint': 'sensor/records/delete/<str:_id>',
             'description': 'Deletes one sensor record by ID',
-            'parameters': [
-                {
-                    'name': '_id',
-                    'datatype': 'str',
-                    'required': True,
-                },
-            ],
-            'example': 'sensor/record/delete?_id=IPKR8IF8BCVZ',
+            'parameters': [],
+            'example': 'sensor/records/delete/IPKR8IF8BCVZ',
             'methods': ['DELETE'],
         },
     ]
@@ -123,37 +111,20 @@ def all_sensor_records(request):
 
 
 @api_view(['GET'])
-def get_sensor_record(request):
-    record_id = request.GET['_id']
+def get_sensor_record(request, _id):
+    record_id = str(_id)
     record = crud_ops.get_sensor_record(record_id=record_id)
     return Response(data=record)
 
 
-@api_view(['GET', 'POST'])
-def add_sensor_record(request):
-    reading = float(request.GET['reading'])
-    sensor_type = str(request.GET['sensorType'])
-    crud_ops.add_sensor_record(reading=reading, sensor_type=sensor_type)
-    response = {"message": "Record was added successfully", "status_code": 201}
-    return Response(data=response)
-
-
-@api_view(['GET', 'DELETE'])
-def delete_sensor_record(request):
-    record_id = str(request.GET['_id'])
-    crud_ops.delete_sensor_record(record_id=record_id)
-    response = {"message": "Record was deleted successfully", "status_code": 200}
-    return Response(data=response)
-
-
 @api_view(['GET'])
 def filter_sensor_records(request):
-    records = crud_ops.get_all_records_from_mongodb(collection_name=config.MONGODB_COLLECTION_SENSOR_DATA)
     sensor_type = request.GET.get('sensorType', default=None)
     min_reading = request.GET.get('minReading', default=None)
     max_reading = request.GET.get('maxReading', default=None)
     start_date = request.GET.get('startDate', default=None)
     end_date = request.GET.get('endDate', default=None)
+    records = crud_ops.get_all_records_from_mongodb(collection_name=config.MONGODB_COLLECTION_SENSOR_DATA)
     records = filters.filter_sensor_records(records=records,
                                             sensor_type=sensor_type,
                                             min_reading=float(min_reading) if min_reading else None,
@@ -167,5 +138,24 @@ def filter_sensor_records(request):
 def sensor_stats(request):
     start_date = request.GET['startDate']
     end_date = request.GET['endDate']
-    dictionary_stats = stat_calc.get_sensor_statistics(start_date=start_date, end_date=end_date)
+    dictionary_stats = stat_calc.get_sensor_statistics(start_date=start_date,
+                                                       end_date=end_date)
     return Response(data=dictionary_stats)
+
+
+@api_view(['GET', 'POST'])
+def add_sensor_record(request):
+    reading = float(request.GET['reading'])
+    sensor_type = str(request.GET['sensorType'])
+    crud_ops.add_sensor_record(reading=reading,
+                               sensor_type=sensor_type)
+    response = {"message": "Record was added successfully", "status_code": 201}
+    return Response(data=response)
+
+
+@api_view(['GET', 'DELETE'])
+def delete_sensor_record(request, _id):
+    record_id = str(_id)
+    crud_ops.delete_sensor_record(record_id=record_id)
+    response = {"message": "Record was deleted successfully", "status_code": 200}
+    return Response(data=response)
