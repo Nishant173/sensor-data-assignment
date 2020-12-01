5ceda72b0951d7d9c7c7178c3ba6e2c9cd997a72
nishant173 <nishant.rao173@gmail.com>
2020-12-01 20:51:32 +0530

    Re-ordered URLs (fixes error for /api/sensor/records/<str:_id>)

--- src/sensor_api/api/urls.py
+++ src/sensor_api/api/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path(route='', view=views.api_home, name='api-home'),
     path(route='documentation', view=views.documentation, name='documentation'),
     path(route='sensor/records', view=views.all_sensor_records, name='all-sensor-records'),
-    path(route='sensor/records/<str:_id>', view=views.get_sensor_record, name='get-sensor-record'),
     path(route='sensor/records/filter', view=views.filter_sensor_records, name='filter-sensor-records'),
     path(route='sensor/records/stats', view=views.sensor_stats, name='sensor-stats'),
     path(route='sensor/records/add', view=views.add_sensor_record, name='add-sensor-record'),
     path(route='sensor/records/delete/<str:_id>', view=views.delete_sensor_record, name='delete-sensor-record'),
+    path(route='sensor/records/<str:_id>', view=views.get_sensor_record, name='get-sensor-record'),
 ]
